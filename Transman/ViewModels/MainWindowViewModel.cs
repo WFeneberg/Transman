eea7bf1823ba3580c210c0340b3ebfe1d60d2bb5
WFeneberg <WFeneberg@yahoo.de>
2024-11-13 22:59:44 +0100

    Demo DbContext implementation

--- Transman/ViewModels/MainWindowViewModel.cs
+++ Transman/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,17 @@
-﻿namespace Transman.ViewModels;
+﻿using System.Linq;
+using Transman.Data.Persons;
+
+namespace Transman.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
     public string Greeting => "Oberflaeche kommt noch, Backend geht vor";
+ 
+    //Demo DbContext 
+    PersonDbContext? _personDbContext = null!;
+    public MainWindowViewModel()
+    {
+        _personDbContext = App.Current?.Services?.GetService(typeof(PersonDbContext)) as PersonDbContext;
+        var personlist = _personDbContext?.Persons.ToList();
+    }
 }
