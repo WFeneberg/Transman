ee229751c0b682ca7652b4a1b374b35837a7105c
WFeneberg <WFeneberg@yahoo.de>
2024-11-20 13:45:44 +0100

    Beginning sidebar menu with reactive ui

--- Transman/ViewModels/MainWindowViewModel.cs
+++ Transman/ViewModels/MainWindowViewModel.cs
@@ -1,17 +1,38 @@
 ﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ReactiveUI;
 using Transman.Persons;
 
 namespace Transman.ViewModels;
 
-public class MainWindowViewModel : ViewModelBase
+public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Oberflaeche kommt noch, Backend geht vor";
- 
+
     //Demo DbContext 
     PersonDbContext? _personDbContext = null!;
+
     public MainWindowViewModel()
     {
         _personDbContext = App.Current?.Services?.GetService(typeof(PersonDbContext)) as PersonDbContext;
         var personlist = _personDbContext?.Persons.ToList();
+
+        TriggerPaneCommand = ReactiveCommand.Create(TriggerPane);
+    }
+
+    public ICommand TriggerPaneCommand { get; }
+
+    private bool _isPaneOpen;
+
+    public bool IsPaneOpen
+    {
+        get => _isPaneOpen;
+        set => this.RaiseAndSetIfChanged(ref _isPaneOpen, value);
+    }
+
+    private Task TriggerPane()
+    {
+        IsPaneOpen = !IsPaneOpen;
+        return Task.CompletedTask;
     }
 }
